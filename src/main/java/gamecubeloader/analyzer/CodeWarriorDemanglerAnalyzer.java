bb473c39212a6637f3963f6d02f577312506b2b2
Cuyler36 <jeremy96@vt.edu>
2021-12-16 01:29:35 -0500

    CodeWarriorDemangler: Add Demangler Analyzer
    This adds a context menu option under Analysis that demangles a symbol/function using the CodeWarrior demangler.

---
+++ src/main/java/gamecubeloader/analyzer/CodeWarriorDemanglerAnalyzer.java
@@ -0,0 +1,41 @@
+package gamecubeloader.analyzer;
+
+import gamecubeloader.common.CodeWarriorDemangler;
+import ghidra.app.plugin.core.analysis.AbstractDemanglerAnalyzer;
+import ghidra.app.util.demangler.DemangledException;
+import ghidra.app.util.demangler.DemangledObject;
+import ghidra.app.util.demangler.DemanglerOptions;
+import ghidra.app.util.importer.MessageLog;
+import ghidra.program.model.listing.Program;
+
+/***
+ * 
+ * Demangler analyzer for CodeWarrior symbols.
+ */
+public class CodeWarriorDemanglerAnalyzer extends AbstractDemanglerAnalyzer {
+
+    private static final String NAME = "Demangle CodeWarrior";
+    private static final String DESCRIPTION =
+        "After a function is created, this analyzer will attempt to demangle " +
+            "the name and apply datatypes to parameters.";
+
+    private CodeWarriorDemangler demangler = new CodeWarriorDemangler();
+    
+    public CodeWarriorDemanglerAnalyzer() {
+        super(NAME, DESCRIPTION);
+        setDefaultEnablement(true);
+    }
+
+    @Override
+    public boolean canAnalyze(Program program) {
+        return demangler.canDemangle(program);
+    }
+
+    @Override
+    protected DemangledObject doDemangle(String mangled, DemanglerOptions options, MessageLog log)
+            throws DemangledException {
+        DemangledObject demangled = demangler.demangle(mangled, options);
+        return demangled;
+    }
+
+}
